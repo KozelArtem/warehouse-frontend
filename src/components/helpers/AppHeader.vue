f9ebf685447ba10506df4eb38637604406218491
Artem Kozel <kozel.as@ya.ru>
2019-11-25 21:55:33 +0300

    Header. Added app header with urls.

---
+++ src/components/helpers/AppHeader.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-card color="teal lighten-4" flat tile>
+    <v-toolbar dense color="teal darken-3">
+      <!-- <v-toolbar-title class="mr-5 ml-2 white--text">MPP</v-toolbar-title> -->
+      <v-toolbar-items v-if="isLoggedIn">
+        <v-tooltip bottom color="teal"
+          nudge-bottom="-20" v-for="item in menu" :key="item.id">
+          <template v-slot:activator="{ on }">
+            <router-link :to="item.link" v-slot="{ isExactActive, navigate }">
+              <v-icon
+                class="px-3"
+                medium
+                :color="isExactActive ? 'deep-orange accent-2' : 'white'"
+                v-on="on"
+                @click="navigate"
+              >
+                {{ item.icon }}
+              </v-icon>
+            </router-link>
+          </template>
+          <span class="white--text">{{ item.title }}</span>
+        </v-tooltip>
+      </v-toolbar-items>
+      <v-spacer />
+      <v-icon v-if="!isLoggedIn" color="green" icon @click="signIn" dark>mdi-login</v-icon>
+      <v-icon v-else color="red" class="mr-4" icon @click="signOut" dark>mdi-logout</v-icon>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+import api from '@/api';
+
+const {
+  isLoggedIn,
+} = api;
+
+export default {
+  data: () => ({
+    menu: [
+      {
+        id: 0,
+        link: '/',
+        icon: 'mdi-calendar',
+        title: 'Заказы',
+      },
+      {
+        id: 1,
+        link: '/warehouse',
+        icon: 'mdi-warehouse',
+        title: 'Склад',
+      },
+      {
+        id: 2,
+        link: '/waybill',
+        icon: 'mdi-file-document-box-check-outline',
+        title: 'Накладные',
+      },
+      {
+        id: 4,
+        link: '/report',
+        icon: 'mdi-file-chart-outline',
+        title: 'Отчеты',
+      },
+      {
+        id: 5,
+        link: '/distribution',
+        icon: 'mdi-settings-transfer',
+        title: 'Журнал работ',
+      },
+      {
+        id: 6,
+        link: '/company',
+        icon: 'mdi-domain',
+        title: 'Компании',
+      },
+      {
+        id: 7,
+        link: '/',
+        icon: 'mdi-book-search',
+        title: 'Дневник',
+      },
+    ],
+  }),
+
+  computed: {
+    isLoggedIn() {
+      return isLoggedIn();
+    },
+  },
+
+  methods: {
+    signIn() {
+      this.$router.push('/login');
+    },
+
+    signOut() {
+      localStorage.removeItem('token');
+      window.location.reload(true);
+    },
+  },
+};
+</script>

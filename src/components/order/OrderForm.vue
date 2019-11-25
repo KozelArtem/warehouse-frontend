f22be131fbf5d48628d5af32aca227d5e258fc5b
Artem Kozel <kozel.as@ya.ru>
2019-11-25 22:43:02 +0300

    Order. Added views for create/update, list.

---
+++ src/components/order/OrderForm.vue
@@ -0,0 +1,254 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <QuickCreateItemModal
+      :dialog="newItemDialog"
+      @close="newItemDialog = false"
+      @submit="onNewItem"
+    />
+    <CompanyModal
+      :dialog="showCompanyDialog"
+      @close="showCompanyDialog = false"
+      @submit="onNewCompany"
+    />
+    <v-card>
+      <v-card-title class="purple darken-3 white--text">
+        <span class="headline">Заказы</span>
+        <v-spacer></v-spacer>
+        <v-icon color="red" @click="onClose()">{{icons.close}}</v-icon>
+      </v-card-title>
+      <v-card-text>
+        <v-form v-model="valid">
+          <v-container grid-list-md>
+            <v-layout column wrap>
+              <v-flex>
+                <AutocompleteWithAdd
+                  label="Наименование"
+                  :items="itemsNames"
+                  :loading="loadingItems"
+                  :slotButtonDisabled="newItemDialog"
+                  :selectedItemId="order.itemId"
+                  @slotButtonClick="newItemDialog = true"
+                  @change="onItemSelect"
+                />
+              </v-flex>
+              <v-flex>
+                <AutocompleteWithAdd
+                  label="Компания"
+                  :items="companies"
+                  :loading="loadingCompanies"
+                  :slotButtonDisabled="showCompanyDialog"
+                  :selectedItemId="order.companyId"
+                  @slotButtonClick="showCompanyDialog = true"
+                  @change="companyId => order.companyId = companyId"
+                />
+              </v-flex>
+              <v-flex>
+                <v-text-field
+                  label="Количество"
+                  v-model="order.orderAmount"
+                  hide-details
+                  dense
+                  type="number"
+                  :rules="[required, positiveNumber]"
+                />
+              </v-flex>
+              <v-flex>
+                <v-menu
+                v-model="datePickerMenu"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      v-model="order.date"
+                      label="Дата"
+                      hide-details
+                      dense
+                      readonly
+                      v-on="on"
+                    />
+                  </template>
+                  <v-date-picker
+                    v-model="order.date"
+                    @input="datePickerMenu = false"
+                    locale="ru-RU"
+                  />
+                </v-menu>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          small
+          color="success"
+          :dark="valid"
+          :loading="formLoading"
+          :disabled="!valid"
+          @click="onSave()"
+        >Сохранить</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import api from '../../api';
+import constants from '../../constants/data.json';
+
+import dates from '../../helpers/dates';
+import rules from '../../helpers/validationRules';
+
+import AutocompleteWithAdd from '../helpers/AutocompleteWithAdd.vue';
+import QuickCreateItemModal from '../item/QuickCreateItemModal.vue';
+import CompanyModal from '../company/CompanyModal.vue';
+
+const {
+  updateItem,
+  createOrder,
+  getItemList,
+  getItemShortInfo,
+  getShortCompanyList,
+} = api;
+
+const {
+  icons,
+} = constants;
+
+const {
+  format: formatDate,
+} = dates;
+
+
+export default {
+  components: {
+    AutocompleteWithAdd,
+    QuickCreateItemModal,
+    CompanyModal,
+  },
+
+  props: {
+    dialog: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+
+  data: () => ({
+    icons,
+    ...rules,
+
+    itemsLoading: false,
+    newItemDialog: false,
+    showCompanyDialog: false,
+
+    loadingItems: false,
+    items: [],
+
+    loadingCompanies: false,
+    companies: [],
+
+    orderTemplate: {
+      itemId: 0,
+      companyId: 0,
+      date: formatDate(Date.now(), 'YYYY-MM-DD'),
+      amount: 0,
+    },
+
+    order: {},
+    datePickerMenu: false,
+    valid: false,
+
+    loading: false,
+    formLoading: false,
+  }),
+
+  computed: {
+    itemsNames() {
+      return this.items.map(value => ({
+        // TODO show category on hover
+        id: value.id,
+        name: `${value.categoryName} - ${value.name}`,
+      }));
+    },
+  },
+
+  async beforeMount() {
+    this.order = { ...this.orderTemplate };
+
+    this.loadItemList();
+    this.loadCompanyList();
+  },
+
+  methods: {
+    formatDate,
+
+    async loadItemList() {
+      this.itemsLoading = true;
+      this.items = await getItemList();
+      this.itemsLoading = false;
+    },
+
+    async loadCompanyList() {
+      this.loadingCompanies = true;
+      this.companies = await getShortCompanyList();
+      this.loadingCompanies = false;
+    },
+
+    async onNewItem(newItem) {
+      const item = await getItemShortInfo(newItem.id);
+      this.items.push(item);
+      this.order.itemId = item.id;
+      this.order.companyId = item.companyId || 0;
+      this.newItemDialog = false;
+    },
+
+    async onNewCompany(company) {
+      this.companies.push(company);
+      await updateItem(this.order.itemId, { companyId: company.id });
+
+      this.order.companyId = company.id;
+      this.showCompanyDialog = false;
+    },
+
+    onItemSelect(itemId) {
+      const item = this.items.filter(value => value.id === itemId)[0] || {};
+
+      this.order.itemId = itemId;
+      this.order.companyId = item.companyId || 0;
+    },
+
+    async onSave() {
+      this.formLoading = true;
+
+      const item = this.items.filter(value => value.id === this.order.itemId)[0] || {};
+      const newCompanyId = this.order.companyId;
+
+      if (newCompanyId !== item.companyId) {
+        await updateItem(this.order.itemId, { companyId: newCompanyId });
+      }
+
+      delete this.order.companyId;
+
+      const result = await createOrder(this.order);
+
+      this.order = { ...this.orderTemplate };
+      this.formLoading = false;
+
+      this.$emit('submit', result.id);
+    },
+
+    onClose() {
+      this.order = { ...this.orderTemplate };
+
+      this.$emit('close');
+    },
+  },
+};
+</script>
---
+++ src/views/OrderListTable.vue
@@ -0,0 +1,271 @@
+<template>
+  <v-container grid-list-xs>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-data-table
+          dense
+          :headers="headers"
+          :items="sortedOrders"
+          :loading="loading"
+          :loading-text="constants.loading"
+          :no-results-text="constants.noResults"
+          :no-data-text="constants.noData"
+          @click:row="selectRow"
+        >
+          <template v-slot:top>
+            <v-toolbar color="">
+              <v-toolbar-title>{{ title }}</v-toolbar-title>
+              <v-divider class="mx-4" inset vertical></v-divider>
+                <span style="width: 200px">
+                  <v-select
+                    v-model="activeOrderSortId"
+                    :items="statuses"
+                    menu-props="auto"
+                    label="Статус"
+                    hide-details
+                    item-value="id"
+                    item-text="name"
+                    prepend-icon="mdi-sort"
+                    single-line
+                  ></v-select>
+                </span>
+              <v-divider class="mx-4" inset vertical></v-divider>
+              <v-spacer></v-spacer>
+              <v-dialog persistent v-if="isAdmin()" v-model="dialog" max-width="80%">
+                <template v-slot:activator="{ on }">
+                  <v-icon medium v-on="on" color="green" class="mr-1">
+                    mdi-square-edit-outline</v-icon>
+                </template>
+                <OrderForm :dialog="dialog" @submit="onSubmit" @close="dialog = false" />
+              </v-dialog>
+            </v-toolbar>
+          </template>
+
+          <template v-slot:header.name="{ header }">
+            {{ header.text }}
+            <v-tooltip bottom v-if="isAdmin()">
+              <template v-slot:activator="{ on }">
+                <v-icon color="green" dark small v-on="on">mdi-information</v-icon>
+              </template>
+              <span>
+                Клик - редактирование <br/>
+                Правая - удаление
+              </span>
+            </v-tooltip>
+          </template>
+
+          <template v-slot:item.name="{ item }">
+            <span @contextmenu.prevent="deleteOrder(item)">{{ item.item.name }}</span>
+          </template>
+
+          <template v-slot:item.amount="{ item }">
+            <span>{{ item.orderAmount }} / {{ item.amount }}</span>
+          </template>
+
+          <template v-slot:item.status="{ item }">
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-icon
+                  color="green"
+                  small
+                  v-on="on"
+                  @click.stop="companyId = item.item.companyId; companyDialog = true"
+                >
+                  mdi-information
+                </v-icon>
+              </template>
+              <span>Информация о компании</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-icon :color="item.status.color" small v-on="on">{{ item.status.icon }}</v-icon>
+              </template>
+              <span>Статус заказа: {{ item.status.name }}</span>
+            </v-tooltip>
+          </template>
+        </v-data-table>
+      </v-flex>
+      <v-flex xs12>
+        <CompanyInfoModal
+          :companyId="companyId || 0"
+          :dialog="companyDialog"
+          @close="companyDialog = false"
+        />
+        <EditOrderFormModal
+          :order="editedOrder"
+          :dialog="editOrderDialog"
+          @submit="onEditSubmit"
+          @close="editOrderDialog = false"
+        />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import api from '../api';
+import constants from '../constants/data.json';
+import dates from '../helpers/dates';
+
+import OrderForm from '../components/order/OrderForm.vue';
+import EditOrderFormModal from '../components/order/EditOrderFormModal.vue';
+import CompanyInfoModal from '../components/company/CompanyInfoModal.vue';
+
+const {
+  isAdmin,
+  getOrders,
+  getOrdersByIds,
+  removeOrder,
+} = api;
+
+const {
+  format: formatDate,
+  sortDesc,
+} = dates;
+
+const orderStatuses = {
+  all: {
+    id: 0,
+    name: 'Все',
+  },
+  active: {
+    id: 1,
+    name: 'В ожидании',
+    icon: 'mdi-calendar-clock',
+    color: 'orange',
+  },
+  completed: {
+    id: 2,
+    name: 'Завершенные',
+    icon: 'mdi-check',
+    color: 'green',
+  },
+};
+
+export default {
+  components: {
+    OrderForm,
+    EditOrderFormModal,
+    CompanyInfoModal,
+  },
+
+  data: () => ({
+    selectedRow: null,
+    editRow: false,
+    constants,
+    activeOrderSortId: orderStatuses.active.id,
+
+    headers: [
+      { text: '№', value: 'index' },
+      { text: 'Наименование', value: 'name', sortable: false },
+      { text: 'Заказано / Пришло', value: 'amount', sortable: false },
+      { text: 'Дата', value: 'date', sort: sortDesc },
+      { text: 'Статус', value: 'status', sortable: false },
+    ],
+
+    title: 'Заказы',
+
+    loading: false,
+    orders: [],
+    selected: [],
+    dialog: false,
+
+    companyId: -1,
+    companyDialog: false,
+
+    editedOrder: {},
+    editOrderDialog: false,
+  }),
+
+  computed: {
+    statuses() {
+      return Object.values(orderStatuses);
+    },
+
+    sortedOrders() {
+      let buff = [];
+
+      if (this.activeOrderSortId === orderStatuses.active.id) {
+        buff = this.orders.filter(value => !value.amount);
+      } else if (this.activeOrderSortId === orderStatuses.completed.id) {
+        buff = this.orders.filter(value => !!value.amount);
+      } else {
+        buff = this.orders;
+      }
+
+      return buff
+        .sort(sortDesc)
+        .map((value, index) => {
+          const formatedDate = formatDate(value.date);
+          const data = {
+            ...value,
+            index: index + 1,
+            date: formatedDate,
+            status: this.getStatus(value),
+          };
+
+          return data;
+        });
+    },
+  },
+
+  async beforeMount() {
+    this.orders = await getOrders();
+  },
+
+  methods: {
+    isAdmin,
+
+    selectRow(order) {
+      this.editedOrder = order;
+      this.editOrderDialog = true;
+    },
+
+    getStatus(item) {
+      if (!item.amount) {
+        return orderStatuses.active;
+      }
+
+      return orderStatuses.completed;
+    },
+
+    async onSubmit(orderId) {
+      this.dialog = false;
+
+      const orders = await getOrdersByIds(orderId);
+
+      if (orders.length) {
+        this.orders.push(...orders);
+      }
+    },
+
+    onEditSubmit(order) {
+      const copy = [...this.orders];
+      const index = this.orders.findIndex(value => value.id === order.id);
+
+      copy[index] = order;
+      this.orders = copy;
+      this.editedOrder = {};
+      this.editOrderDialog = false;
+    },
+
+    async deleteOrder(order) {
+      if (!isAdmin()) {
+        return;
+      }
+      // eslint-disable-next-line no-restricted-globals
+      const result = confirm(`Вы дейстивительно хотите удалить ${order.item.name}?`);
+
+      if (!result) {
+        return;
+      }
+
+      const success = await removeOrder(order.id);
+
+      if (success) {
+        this.orders = this.orders.filter(value => value.id !== order.id);
+      }
+    },
+  },
+};
+</script>

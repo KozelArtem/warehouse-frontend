ef60614b59c0e51ebf8fd8638443603cf78d2d67
Artem Kozel <kozel.as@ya.ru>
2019-12-03 21:12:32 +0300

    Waybill. Updated view.

--- src/components/waybill/WaybillList.vue
+++ src/components/waybill/WaybillList.vue
@@ -1,77 +1,76 @@
 <template>
-  <div>
-    <v-dialog
-      v-model="newWaybillDialog"
-      scrollable fullscreen
-      persistent no-click-animation
-      transition="dialog-transition"
-    >
-      <v-card>
-        <v-card-title class="purple white--text darken-3">
-          <span class="headline">Добавление накладной</span>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12>
+      <v-dialog
+          v-model="newWaybillDialog"
+          scrollable fullscreen
+          persistent no-click-animation
+          transition="dialog-transition"
+        >
+          <v-card>
+            <v-card-title class="purple white--text darken-3">
+              <span class="headline">Добавление накладной</span>
+              <v-spacer></v-spacer>
+              <v-icon color="red" @click="newWaybillDialog = false">mdi-close</v-icon>
+            </v-card-title>
+            <v-card-text>
+              <WaybillForm @submit="onWaybillFormSubmit"/>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+      </v-flex>
+      <v-flex xs12>
+        <v-toolbar class="elevation-3 purple darken-2">
+          <v-toolbar-title class="white--text">Накладные</v-toolbar-title>
           <v-spacer></v-spacer>
-          <v-icon color="red" @click="newWaybillDialog = false">mdi-close</v-icon>
-        </v-card-title>
-        <v-card-text>
-          <WaybillForm @submit="onWaybillFormSubmit"/>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
 
-    <v-toolbar class="elevation-0">
-      <v-toolbar-title>Накладные</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon color="green" @click="newWaybillDialog = true">mdi-plus-circle</v-icon>
-      </v-btn>
-    </v-toolbar>
-      <v-divider></v-divider>
-    <v-simple-table dense class="elevation-10">
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">Номер</th>
-            <th class="text-left">Дата</th>
-            <th class="text-left">Количество</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            class="pointer"
-            v-for="waybill in waybills"
-            :key="waybill.id"
-            @click="selectRow(waybill)"
-          >
-            <td>{{ waybill.number }}</td>
-            <td>{{ waybill.date }}</td>
-            <td>{{ waybill.purchaseCount }}</td>
-            <td>
-               <v-icon>mdi-pencil</v-icon>
-            </td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-    <v-dialog
-      v-model="dialog"
-      scrollable fullscreen
-      persistent no-click-animation
-      transition="dialog-transition"
-    >
-      <v-card v-if="selectedWaybill">
-        <v-card-title class="purple white--text darken-3">
-          <span class="headline">
-            {{ `${selectedWaybill.number} от ${selectedWaybill.date}` }}
+          <span :class="['d-flex', {
+            'w-200': $vuetify.breakpoint.smAndUp,
+            'w-100': $vuetify.breakpoint.xs,
+            }
+          ]">
+            <v-text-field prepend-icon="mdi-magnify"
+              dark dense label="Поиск"
+              hide-details v-model="search"></v-text-field>
           </span>
           <v-spacer></v-spacer>
-          <v-icon color="red" @click="dialog = false">mdi-close</v-icon>
-        </v-card-title>
-        <v-card-text>
-          <WaybillInfo :id="selectedWaybill.id" />
-        </v-card-text>
-      </v-card>
-    </v-dialog>
-  </div>
+          <v-btn icon>
+            <v-icon color="green" dark @click="newWaybillDialog = true">mdi-plus-circle</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-divider></v-divider>
+      </v-flex>
+      <v-flex xs6 sm4 md3 lg2 v-for="waybill in items" :key="waybill.id">
+        <v-card class="elevation-5 pa-2 pointer" @click="selectRow(waybill)">
+          <div class="overflow-hidden subtitle-2 text-truncate">
+            {{ waybill.number }}
+          </div>
+          <v-card-subtitle class="subtitle-2 pa-0">{{ waybill.date }}</v-card-subtitle>
+        </v-card>
+      </v-flex>
+
+      <v-dialog
+        v-model="dialog"
+        scrollable fullscreen
+        persistent no-click-animation
+        transition="dialog-transition"
+      >
+        <v-card v-if="selectedWaybill">
+          <v-card-title class="purple white--text darken-3">
+            <span class="headline">
+              {{ `${selectedWaybill.number} от ${selectedWaybill.date}` }}
+            </span>
+            <v-spacer></v-spacer>
+            <v-icon color="red" @click="dialog = false">mdi-close</v-icon>
+          </v-card-title>
+          <v-card-text>
+            <WaybillInfo :id="selectedWaybill.id" />
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -95,6 +94,8 @@ export default {
 
     selectedWaybill: null,
 
+    items: [],
+    search: null,
     waybills: [],
   }),
 
@@ -102,10 +103,18 @@ export default {
     this.loadWaybills();
   },
 
+  watch: {
+    search() {
+      const searchValue = this.search.trim();
+      this.items = this.waybills.filter(waybill => waybill.number.includes(searchValue));
+    },
+  },
+
   methods: {
     async loadWaybills() {
       const data = await getWaybillList();
       this.waybills = data.map(value => ({ ...value, date: formatDate(value.date) }));
+      this.items = this.waybills;
     },
 
     selectRow(waybill) {
@@ -123,4 +132,19 @@ export default {
 </script>
 
 <style lang="scss">
+.w-200 {
+  width: 200px;
+}
+
+.w-100 {
+  width: 100px;
+}
+
+.aaaa {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  width: 160px;
+  height: 1.2em;
+  white-space: nowrap;
+}
 </style>

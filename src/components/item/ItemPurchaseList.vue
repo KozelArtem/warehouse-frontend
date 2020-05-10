2b72baa817bd1d0d8506216155b2d484d467b4d9
GIT Artem Kozel <kozel.as@yandex.ru>
2020-05-10 13:25:18 +0300

    Item view. Updated purchase list view.

--- src/components/item/ItemPurchaseList.vue
+++ src/components/item/ItemPurchaseList.vue
@@ -1,29 +1,43 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="mappedItems"
-    class="elevation-1"
-    item-key="id"
-    :no-data-text="noData"
-    :no-results-text="noResults"
-    locale="ru-RU"
-    dense
-  >
-    <template v-slot:item.waybillNumber="{ item }">
-      <span v-if="!item.waybillNumber">
-        Ожидание
-      </span>
-      <router-link v-else :to="`waybill/${item.waybill.id}`">
-        {{ item.waybillNumber }}
-      </router-link>
-    </template>
-  </v-data-table>
+  <div>
+    <v-alert v-if="!mappedItems.length" type="info" dense outlined :value="true">
+      Нет заказов
+    </v-alert>
+    <v-simple-table v-else dense class="elevation-10">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th
+              class="body-2 font-weight-bold"
+              v-for="(header, i) in headers"
+              :key="`header${i}`"
+              :width="header.width"
+            >
+              {{ header.name }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, i) in mappedItems" :key="item.id">
+            <td>{{ i + 1 }}</td>
+            <td>
+              <span v-if="!item.waybillNumber">
+                Ожидание
+              </span>
+              <router-link v-else :to="`waybill/${item.waybill.id}`">
+                {{ item.waybillNumber }}
+              </router-link>
+            </td>
+            <td>{{ item.date | date }}</td>
+            <td>{{ item.amount }} </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
 </template>
 
 <script>
-import { format as formatDate, sortDesc } from '../../helpers/dates';
-
-import constant from '../../constants/data.json';
 
 export default {
   props: {
@@ -35,44 +49,21 @@ export default {
 
   data: () => ({
     headers: [
-      {
-        text: '№',
-        value: 'index',
-        width: '20px',
-        divider: true,
-      },
-      {
-        text: 'Накладная',
-        value: 'waybillNumber',
-        sortable: false,
-        divider: true,
-      },
-      {
-        text: 'Дата',
-        value: 'date',
-        divider: true,
-        sort: sortDesc,
-      },
-      {
-        text: 'Количество',
-        value: 'amount',
-        sortable: false,
-        divider: true,
-      },
+      { name: '№', width: '10%' },
+      { name: 'Накладная', width: '30%' },
+      { name: 'Дата', width: '30%' },
+      { name: 'Количество', width: '20%' },
     ],
-    ...constant,
   }),
 
   computed: {
     mappedItems() {
       return this.items.map((item, index) => {
         const amount = item.amount > 0 ? item.amount : item.orderAmount;
-        const date = formatDate(item.date);
 
         return {
           ...item,
           index: index + 1,
-          date,
           amount,
           waybillNumber: (item.waybill || {}).number,
         };
@@ -81,7 +72,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-
-</style>
